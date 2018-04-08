c86d0c0622449cb8426fc7dd01137211168f5612
Piotr Dubiel <piotrdubiel@icloud.com>
2018-04-08 23:33:05 +0200

    Lesson 24

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb://piotrek:test123@ds227459.mlab.com:27459/gql-data');
+
+mongoose.connection.once('open', () => {
+  console.log('connected to the database');
+});
+
+// bind express with graphql
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
+app.listen(4000, () => {
+    console.log('now listening for requests on port 4000');
+});
